40c7cacdbf80138da620a979d2f3edf828a7a1a1
nandhinibalaji311 <nandhinibalaji311@gmail.com>
2021-04-11 11:58:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myremindme/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.myremindme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
