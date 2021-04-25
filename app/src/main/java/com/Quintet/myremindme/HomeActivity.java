5e66bf46e093bf9c901c9f8470f9a5873261f59f
Chamanth Krishna <chamanthyarlagadda@gmail.com>
2021-04-25 23:15:45 +0530

    Created Home activity and Add task UI

--- app/src/main/java/com/Quintet/myremindme/HomeActivity.java
+++ app/src/main/java/com/Quintet/myremindme/HomeActivity.java
@@ -3,6 +3,9 @@ package com.Quintet.myremindme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
